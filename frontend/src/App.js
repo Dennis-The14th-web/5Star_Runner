476f21abe8d2d3365e8bda267533a3ea01243127
louis mane <dennis.itua1@gmail.com>
2021-05-07 03:36:30 -0400

    updated app.js component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Movies from './components/Movies';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Movies />
     </div>
   );
 }
