4c8207729eadab5ba239aa8055dbcc7a254f89d4
louis mane <dennis.itua1@gmail.com>
2021-05-07 03:55:02 -0400

    updated footer component

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,2 +1,18 @@
+import React from 'react';
+import './index.css';
+
+const Footer = ()=> {
+
+        return (
+            <div className="footer fixed-bottom ">
+            <div>
+                <a className="fa fa-linkedin-square" href="https://www.linkedin.com/in/dennis-itua-975b9055/"></a>
+                <a className="fa fa-github" href="https://github.com/Dennis-The14th-web/5Star_Runner"></a>
+                <p><small>React frontend application <b>&#9400; 2021 Dennis Itua </b></small></p> 
+            </div>            
+            </div>
+        )
+}
 
 
+export default Footer;
