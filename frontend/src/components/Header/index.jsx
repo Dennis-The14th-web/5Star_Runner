4d4fa6123b1433daec8c2f62ecdedc007d29f07b
louis mane <dennis.itua1@gmail.com>
2021-05-07 03:28:25 -0400

    updated header component

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './index.css'
+
+
+function Header() {
+    return (
+        <div> 
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <h3 className="navbar-brand"><small id="small">5Star</small> <b id="bold">Runners</b></h3>
+                    <div className="input-group">
+                        <span className="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                            </svg>
+                        </span>
+                        <input type="text" className="form-control" placeholder="Search for a movie" aria-label="Username" aria-describedby="basic-addon1" />
+                    </div>
+
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
