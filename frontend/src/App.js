f210968b105ca03ed6415ebe486ba8a2d4fe34c8
louis mane <dennis.itua1@gmail.com>
2021-05-07 04:00:46 -0400

    updated app.js with the footer child component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Movies from './components/Movies';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Movies />
+      <Footer />
     </div>
   );
 }
