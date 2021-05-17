6beebeb443347b42c0fe9f694fe18a161a94ffcd
louis mane <dennis.itua1@gmail.com>
2021-05-17 16:12:32 -0400

    updated footer

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -5,8 +5,8 @@ const Footer = ()=> {
 
         return (
             <div className="footer fixed-bottom bg-light">
-                <span className="fa fa-linkedin-square" href="https://www.linkedin.com/in/dennis-itua-975b9055/"></span>
-                <span className="fa fa-github" href="https://github.com/Dennis-The14th-web/5Star_Runner"></span>
+                <a className="fa fa-linkedin-square" href="https://www.linkedin.com/in/dennis-itua-975b9055/"></a>
+                <a className="fa fa-github" href="https://github.com/Dennis-The14th-web/5Star_Runner"></a>
                 <p><small>React JS frontend application <b>&#9400; 2021 Dennis Itua </b></small></p> 
             </div>
         )
